af0be34a27bed99dd4f9a39504ee1f338236da05
Dima <dvnoskov88@gmail.com>
2023-08-08 20:56:35 +0300

    add requests

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from application.db.people import Employee
 from application.salary import calculate_salary
 from datetime import datetime
+import requests
 
 if __name__ == "__main__":
     new_emp = Employee("Tom", 19)
     print(datetime.today())
     print(new_emp.get_employees())
     print(calculate_salary(1,12))
+
+    x = requests.get('https://netology.ru/')
+    print(x.content[0])
 
